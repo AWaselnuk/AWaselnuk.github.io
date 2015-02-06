f947bee48f0162e61e1044316793531fc23a8c74
Adam Waselnuk <awaselnuk@mygrocerydeals.com>
2015-02-05 19:55:37 -0500

    Completed themes.

--- _coffee/main.coffee
+++ _coffee/main.coffee
@@ -1,8 +1,5 @@
 'use strict'
 
-# Console Easter Egg
-console.log '%c Welcome fellow DOM traveler! ', 'background: #002B36; color: #2AA198;'
-
 # DOM Ready
 ready = (fn) ->
   if document.readyState != 'loading'
@@ -10,20 +7,39 @@ ready = (fn) ->
   else
     document.addEventListener 'DOMContentLoaded', fn
 
+# Detect mobile browser
+mobileBrowser = window.innerWidth <= 800
+
 # Returns a random integer between min (included) and max (excluded)
 getRandomInt = (min, max) ->
   Math.floor(Math.random() * (max - min)) + min
 
 # A collection of themes with a cover image and color
 themes = [
-  {image: 'homepage.jpg', color: '#00C2AF', titleColor: '#174044'},
-  {image: 'homepage.jpg', color: '#00C2AF', titleColor: '#174044'}
+  {image: 'tree.jpg', color: '#00C2AF', titleColor: '#174044'},
+  {image: 'retro-pop.jpg', color: '#ef9c19', titleColor: '#1c1f3b'},
+  {image: 'bridge.jpg', color: '#10d074', titleColor: '#dbfcef'},
+  {image: 'cat.jpg', color: '#ef02a2', titleColor: '#85065c'},
+  {image: 'city-nights.jpg', color: '#f7a73c', titleColor: '#ffefd8'},
+  {image: 'foggy.jpg', color: '#a4d643', titleColor: '#e9e3d8'},
+  {image: 'nebula.jpg', color: '#e8bc4b', titleColor: '#f5f1eb'},
+  {image: 'neon-sign.jpg', color: '#ef349d', titleColor: '#fff'},
+  {image: 'space-ship.jpg', color: '#7fb676', titleColor: '#f6ffe2'},
+  {image: 'sleeping-cat.jpg', color: '#f3a11b', titleColor: '#fff'},
+  {image: 'tiddly-winks.jpg', color: '#c1201c', titleColor: '#fff'},
+  {image: 'typewriter.jpg', color: '#e42f11', titleColor: '#060606'}
 ]
 
 # Sets the theme image on the page
-setThemeImage = (image) ->
-  # TODO : Write this
-  true
+setThemeImage = (image, color) ->
+  return if mobileBrowser
+  styles =
+    "
+      .layout-mag-left {
+        background-image: url(../img/#{image});
+      }
+    "
+  appendStyles(styles)
 
 # Add styles to the DOM
 appendStyles = (styles) ->
@@ -31,8 +47,14 @@ appendStyles = (styles) ->
   styleElement.innerHTML = styles
   document.getElementsByTagName('head')[0].appendChild(styleElement)
 
+
+# Console Easter Egg
+consoleEasterEgg = (color) ->
+  console.log "%c Welcome fellow DOM traveler!", "color: #{color};"
+
 # Sets the theme color on the page
 setThemeColor = (color) ->
+  consoleEasterEgg(color)
   styles =
     "
       .random-color {
@@ -59,6 +81,7 @@ setThemeColor = (color) ->
   appendStyles(styles)
 
 setTitleColor = (color) ->
+  return if mobileBrowser
   styles =
     "
       .masthead .title,
@@ -68,6 +91,7 @@ setTitleColor = (color) ->
       }
     "
   appendStyles(styles)
+
 
 # Returns the value from local storage or saves the passed in value then returns it.
 # This is because I want the same theme to persist across a session
@@ -80,8 +104,8 @@ lockToSession = (key, value) ->
 # Choose a theme with a cover image and color
 selectRandomTheme = (themes) ->
   randomTheme = themes[getRandomInt(0, themes.length)]
-  setThemeImage(lockToSession('themeImage', randomTheme.image))
   setThemeColor(lockToSession('themeColor', randomTheme.color))
+  setThemeImage(lockToSession('themeImage', randomTheme.image))
   setTitleColor(lockToSession('themeTitleColor', randomTheme.titleColor))
 
 # Initialization
--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 (function() {
   'use strict';
-  var appendStyles, getRandomInt, init, lockToSession, ready, selectRandomTheme, setThemeColor, setThemeImage, setTitleColor, themes;
-
-  console.log('%c Welcome fellow DOM traveler! ', 'background: #002B36; color: #2AA198;');
+  var appendStyles, consoleEasterEgg, getRandomInt, init, lockToSession, mobileBrowser, ready, selectRandomTheme, setThemeColor, setThemeImage, setTitleColor, themes;
 
   ready = function(fn) {
     if (document.readyState !== 'loading') {
@@ -12,24 +10,71 @@
     }
   };
 
+  mobileBrowser = window.innerWidth <= 800;
+
   getRandomInt = function(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   };
 
   themes = [
     {
-      image: 'homepage.jpg',
+      image: 'tree.jpg',
       color: '#00C2AF',
       titleColor: '#174044'
     }, {
-      image: 'homepage.jpg',
-      color: '#00C2AF',
-      titleColor: '#174044'
+      image: 'retro-pop.jpg',
+      color: '#ef9c19',
+      titleColor: '#1c1f3b'
+    }, {
+      image: 'bridge.jpg',
+      color: '#10d074',
+      titleColor: '#dbfcef'
+    }, {
+      image: 'cat.jpg',
+      color: '#ef02a2',
+      titleColor: '#85065c'
+    }, {
+      image: 'city-nights.jpg',
+      color: '#f7a73c',
+      titleColor: '#ffefd8'
+    }, {
+      image: 'foggy.jpg',
+      color: '#a4d643',
+      titleColor: '#e9e3d8'
+    }, {
+      image: 'nebula.jpg',
+      color: '#e8bc4b',
+      titleColor: '#f5f1eb'
+    }, {
+      image: 'neon-sign.jpg',
+      color: '#ef349d',
+      titleColor: '#fff'
+    }, {
+      image: 'space-ship.jpg',
+      color: '#7fb676',
+      titleColor: '#f6ffe2'
+    }, {
+      image: 'sleeping-cat.jpg',
+      color: '#f3a11b',
+      titleColor: '#fff'
+    }, {
+      image: 'tiddly-winks.jpg',
+      color: '#c1201c',
+      titleColor: '#fff'
+    }, {
+      image: 'typewriter.jpg',
+      color: '#e42f11',
+      titleColor: '#060606'
     }
   ];
 
-  setThemeImage = function(image) {
-    return true;
+  setThemeImage = function(image, color) {
+    var styles;
+    if (mobileBrowser) {
+      return;
+    }
+    styles = ".layout-mag-left { background-image: url(../img/" + image + "); }";
+    return appendStyles(styles);
   };
 
   appendStyles = function(styles) {
@@ -39,14 +84,22 @@
     return document.getElementsByTagName('head')[0].appendChild(styleElement);
   };
 
+  consoleEasterEgg = function(color) {
+    return console.log("%c Welcome fellow DOM traveler!", "color: " + color + ";");
+  };
+
   setThemeColor = function(color) {
     var styles;
+    consoleEasterEgg(color);
     styles = ".random-color { color: " + color + " !important; } .btn.random-color { background-color: " + color + " !important; } a { color: " + color + " !important; } a:hover { color: " + color + " !important; } a.btn, a.btn-primary, a.btn:hover, a.btn-primary:hover, .read-all a, .read-all a:hover { color: #fff !important; }";
     return appendStyles(styles);
   };
 
   setTitleColor = function(color) {
     var styles;
+    if (mobileBrowser) {
+      return;
+    }
     styles = ".masthead .title, .masthead .subtitle, .masthead .intro { color: " + color + " }";
     return appendStyles(styles);
   };
@@ -64,8 +117,8 @@
   selectRandomTheme = function(themes) {
     var randomTheme;
     randomTheme = themes[getRandomInt(0, themes.length)];
-    setThemeImage(lockToSession('themeImage', randomTheme.image));
     setThemeColor(lockToSession('themeColor', randomTheme.color));
+    setThemeImage(lockToSession('themeImage', randomTheme.image));
     return setTitleColor(lockToSession('themeTitleColor', randomTheme.titleColor));
   };
 
